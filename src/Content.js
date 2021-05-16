ef1cda04ab2244fcc0bd08e3bab5837efd8d9711
WuPaulo <paulowbusiness@gmail.com>
2021-05-16 01:06:27 -0400

    firstcommit

---
+++ src/Content.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Part from "./Part";
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part parts={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part parts={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part parts={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </div>
+  );
+};
+
+export default Content;
